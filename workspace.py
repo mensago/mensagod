c737ac0aaf844690089955f70ee5f4fabf178f6c
Jon Yoder <jsyoder@mailfence.com>
2019-06-09 17:27:51 -0400

    Add user management

--- workspace.py
+++ workspace.py
@@ -1,5 +1,6 @@
 import log
 from serverconfig import gConfig
+import user
 
 import json
 import os
@@ -7,29 +8,55 @@ from os import path as path
 import secrets
 import uuid
 
-def generate_session_id(alphabet):
-	# Creates a nice long string of random printable characters to function
-	# as a one-time session ID.
-	return ''.join(secrets.choice(alphabet) for _ in range(64))
-
 class Workspace:
 	def __init__(self):
 		self.id = None
 		self.quota = gConfig['default_quota']
-		self.devices = {}
+		self.users = list()
 		self.status = 'active'
 		
 		# These paths are referenced a lot, so pregenerate them
 		self.workspace_path = None
 		self.system_path = None
-		self.keyring_path = None
+		self.users_path = None
 	
+	def add_user(self, uid, role, devid):
+		'''
+		Adds a user to the workspace, assigns the appropriate role, and associates a first device 
+		with that user. The method returns a session ID to be returned to the user's primary device 
+		for the next session. If an error occurs, None is returned instead.
+		'''
+		if not uid or not devid:
+			raise ValueError("Null uid or devid passed to Workspace::add_user")
+		
+		new_user = user.User(self.id)
+		if uid in self.users:
+			# An error is logged by load() if there is a problem
+			if not new_user.load(uid):
+				return None
+			
+			if new_user.has_device(devid):
+				return new_user.session_for_device(devid)
+			else:
+				sid = new_user.add_device(devid)
+				new_user.save()
+				return sid
+		
+		new_user.role = user.Role(role)
+		sid = new_user.add_device(devid)
+		if not sid:
+			return None
+		
+		if new_user.save():
+			return sid
+		return None
+
 	def ensure_directories(self):
 		'''Makes sure that all directories needed for managing a workspace'''
 		directories = [
 			self.workspace_path,
 			self.system_path,
-			self.keyring_path
+			self.users_path
 		]
 
 		for d in directories:
@@ -54,55 +81,80 @@ class Workspace:
 
 	def generate(self):
 		self.id = str(uuid.uuid4())
-		
-		device_id = str(uuid.uuid4())
-		session_id = generate_session_id(gConfig['session_id_alphabet'])
-		self.devices = { device_id : session_id }
+		self.users = list()
 
 		self.workspace_path = path.join(gConfig['workspacedir'], self.id)
 		self.system_path = path.join(self.workspace_path,'system')
-		self.keyring_path = path.join(self.system_path,'keyring')
+		self.users_path = path.join(self.system_path,'users')
+
+	def has_user(self, uid):
+		if uid in self.users:
+			return True
+		return False
 
 	def load(self, wid):
-		configfile = path.join(gConfig['workspacedir'], wid, 'userconfig.json')
+		configfile = path.join(gConfig['workspacedir'], wid, 'config.json')
 		try:
 			with open(configfile, "r") as read_file:
 				data = json.load(read_file)
 				self.id = data['id']
 				self.quota = data['quota']
-				self.devices = data['devices']
 				self.status = data['status']
+			self.users = [f for f in os.listdir(self.users_path) if \
+							path.isfile(path.join(self.users_path, f))]
+				
 		except Exception as e:
 			log.Log("Couldn't read user config file %s. Using defaults. Exception: %s" % \
 					(configfile, e), log.ERRORS)
 			self.id = None
 			self.quota = gConfig['default_quota']
-			self.devices = {}
+			self.users = list()
 			self.workspace_path = None
 			self.system_path = None
-			self.keyring_path = None
+			self.users_path = None
 			return False
 		
 		self.workspace_path = path.join(gConfig['workspacedir'], self.id)
 		self.system_path = path.join(self.workspace_path,'system')
-		self.keyring_path = path.join(self.system_path,'keyring')
+		self.users_path = path.join(self.system_path,'keyring')
 		return True
 	
 	def print(self):
 		print("ID: %s" % self.id)
 		print("Quota: %s" % self.quota)
-		print("Devices:")
-		for device in self.devices:
-			print("  %s" % device)
+		print("Users:")
+		for user in self.users:
+			print("  %s" % user)
+
+	def remove_user(self, uid):
+		'''
+		Removes a user from the workspace and all of the user's devices. The method returns True if 
+		successful and False if not.
+		'''
+		if not uid:
+			raise ValueError("Null uid or devid passed to Workspace::remove_user")
+		
+		if uid not in self.users:
+			return False
+		
+		del self.users[uid]
+		user_path = path.join(self.users_path, uid)
+		if path.exists(user_path):
+			try:
+				os.remove(user_path)
+			except Exception as e:
+				log.Log("Couldn't remove user file %s. Exception: %s" % \
+						(user_path, e), log.ERRORS)
+				return False
+		return True
 
 	def save(self):
 		outdata = {
 			'id' : self.id,
 			'quota' : self.quota,
-			'devices' : self.devices,
 			'status' : self.status
 		}
-		configfile = path.join(gConfig['workspacedir'], self.id, 'userconfig.json')
+		configfile = path.join(gConfig['workspacedir'], self.id, 'config.json')
 		try:
 			with open(configfile, "w") as handle:
 				json.dump(outdata, handle, ensure_ascii=False, indent=2)
