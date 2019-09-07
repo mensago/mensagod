edcd6a815c77281db86de2aad8f5c229e419a56b
Jon Yoder <jsyoder@mailfence.com>
2019-09-07 11:16:10 -0400

    Add new text encoder/decoder utilities

---
+++ utils/jdec.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+# jdec: a binary-to-text encoding scheme intended specifically for JSON transmission.
+# Based on the yEnc encoder designed by Juergen Helbing
+
+# Steps:
+# For each character, encode only if absolutely necessary. This means NULL, 
+# = (the escape character), and any character needing escaped in JSON.
+
+# Usage: jdec.py <filename> decodes foo.txt.jenc to foo.txt.
+# jdec also decodes stdin to stdout
+
+import array
+import sys
+
+def jdec(indata):
+	outdata = array.array('B')
+	i = 0
+	while(i < len(indata)):
+		if indata[i] == 61:
+			if i + 1 == len(indata):
+				outdata.append((indata[i] - 42) & 255)
+			else:
+				outdata.append((indata[i+1] - 106) & 255)
+				i = i + 1
+		else:
+			outdata.append((indata[i] - 42) & 255)
+		
+		i = i + 1
+		
+	return outdata.tobytes()
+
+def jdec_file(inpath):
+	if len(inpath) < 6 or inpath[-5:] != ".jenc":
+		print("%s is not a .jenc file" % inpath)
+		sys.exit()
+	
+	try:
+		infile = open(inpath, 'r')
+	except:
+		print("Couldn't open %s for decoding. Aborting")
+		sys.exit()
+	
+	outpath = inpath[:-5]
+	try:
+		outfile = open(outpath, 'w')
+	except:
+		print("Couldn't open %s for decoding. Aborting")
+		infile.close()
+		sys.exit()
+	
+	for indata in infile.buffer:
+		outfile.buffer.write(jdec(indata))
+
+	infile.close()
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		for filename in sys.argv[1:]:
+			jdec_file(filename)
+	else:
+		for indata in sys.stdin.buffer:
+			sys.stdout.buffer.write(jdec(indata))
+
---
+++ utils/jenc.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# jEnc: a binary-to-text encoding scheme intended specifically for JSON transmission.
+# Based on the yEnc encoder designed by Juergen Helbing
+
+# Steps:
+# For each character, encode only if absolutely necessary. This means NULL, 
+# = (the escape character), and any character needing escaped in JSON.
+
+# Usage: jenc.py <filename> encodes foo.txt to foo.txt.jenc
+# jenc also encodes stdin to stdout
+
+import array
+import os.path as path
+import sys
+
+def jenc(indata):
+	escapes = [
+		0,	# NULL
+		8,	# Backspace
+		9,	# Tab
+		10, # Line feed
+		12, # Form feed
+		13, # Carriage return
+		34, # Double quotes
+		61, # =, the escape character
+		92, # \, the JSON escape character
+	]
+	outdata = array.array('B')
+	for i in range(0, len(indata)):
+		c = (indata[i] + 42) & 255
+		
+		if c in escapes:
+			outdata.extend([61, c + 64])
+		else:
+			outdata.append(c)
+	return outdata.tobytes()
+
+def jenc_file(inpath):
+	try:
+		infile = open(inpath, 'r')
+	except:
+		print("Couldn't open %s for encoding. Aborting")
+		sys.exit()
+	
+	outpath = inpath + ".jenc"
+	try:
+		outfile = open(outpath, 'w')
+	except:
+		print("Couldn't open %s for encoding. Aborting")
+		infile.close()
+		sys.exit()
+	
+	for indata in infile.buffer:
+		outfile.buffer.write(jenc(indata))
+
+	infile.close()
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		for filename in sys.argv[1:]:
+			jenc_file(filename)
+	else:
+		for indata in sys.stdin.buffer:
+			sys.stdout.buffer.write(jenc(indata))
