9eb4ebb0e3dd565e9d2d84ff21c7fe40b6fdbcdc
Jon Yoder <jsyoder@mailfence.com>
2020-08-02 08:23:01 -0400

    Complete REGISTER integration tests

--- tests/integration/test_register.py
+++ tests/integration/test_register.py
@@ -1,7 +1,7 @@
 
-from integration_setup import setup_test, add_workspace, connect
+from integration_setup import setup_test, connect
 
-# Workspace ID : 63f1a44d-c83f-43b9-9152-9595e110e322
+# Workspace ID : 11111111-1111-1111-1111-111111111111
 # Friendly Address : 
 # Status : active
 # Password : SandstoneAgendaTricycle
@@ -43,12 +43,13 @@ from integration_setup import setup_test, add_workspace, connect
 # Device #5 Private.b85 : !lw2!SA7Qp%u!+(RCbgfdm1xicVHEELHdD!V24je
 
 
-def test_register_success():
-	'''Tests the server's REGISTER command'''
+def test_register_success_dup():
+	'''Tests the server's REGISTER command - success and duplicate WID condition'''
 
+	print("Test: REGISTER")
 	setup_test()
 
-	wid = '63f1a44d-c83f-43b9-9152-9595e110e322'
+	wid = '11111111-1111-1111-1111-111111111111'
 	# password is 'SandstoneAgendaTricycle'
 	pwhash = '$argon2id$v=19$m=65536,t=2,p=1$ew5lqHA5z38za+257DmnTA$0LWVrI2r7XCq' \
 				'dcCYkJLok65qussSyhN5TTZP+OTgzEI'
@@ -58,7 +59,40 @@ def test_register_success():
 	sock = connect()
 	assert sock, "Connection to server at localhost:2001 failed"
 
+	
+	# Test #1: Regular registration that is supposed to succeed
+
 	cmd = ' '.join([ "REGISTER", wid, pwhash, algorithm, devkey, "\r\n" ])
+	print('Regular registration\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s\n' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '201' and parts[1] == 'REGISTERED', 'Failed to register'
+
+	
+	# Test #2: Attempt registration of existing WID
+
+	cmd = ' '.join([ "REGISTER", wid, pwhash, algorithm, devkey, "\r\n" ])
+	print('Duplicate registration\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s\n' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '408' and parts[1] == 'RESOURCE', 'Failed to catch duplicate registration'
+
+
+	# Test #3: Attempt registration with unsupported encryption type
+
+	wid = '11111111-1111-1111-1111-222222222222'
+	cmd = ' '.join([ "REGISTER", wid, pwhash, '3DES', devkey, "\r\n" ])
+	print('Bad encryption algorithm\n--------------------------')
 	print('CLIENT: %s' % cmd)
 	sock.send(cmd.encode())
 
@@ -66,10 +100,122 @@ def test_register_success():
 	print('SERVER: %s' % response)
 	
 	parts = response.split(' ')
-	assert parts[0] == '201' and parts[1] == 'REGISTERED', 'Failed to register'
+	assert parts[0] == '309' and parts[1] == 'ENCRYPTION', 'Failed to catch unsupported algorithm'
 
-	sock.send('QUIT\r\n')
+
+	# Test #4: Send bad WID
+
+	wid = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+	cmd = ' '.join([ "REGISTER", wid, pwhash, '3DES', devkey, "\r\n" ])
+	print('Bad WID\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '400' and parts[1] == 'BAD', 'Failed to catch bad WID'
+
+	# Test #5: Overflow
+
+	wid = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+	cmd = ' '.join([ "REGISTER", wid, pwhash, '3DES', devkey, "\r\n" ])
+	print('Bad WID\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '400' and parts[1] == 'BAD', 'Failed to catch bad WID'
+
+
+	sock.send(b'QUIT\r\n')
+
+
+def test_register_failures():
+	'''Tests the server's REGISTER command with failure conditions'''
+
+	setup_test()
+
+	wid = '11111111-1111-1111-1111-111111111111'
+	# password is 'SandstoneAgendaTricycle'
+	pwhash = '$argon2id$v=19$m=65536,t=2,p=1$ew5lqHA5z38za+257DmnTA$0LWVrI2r7XCq' \
+				'dcCYkJLok65qussSyhN5TTZP+OTgzEI'
+	algorithm = 'curve25519'
+	devkey = '@X~msiMmBq0nsNnn0%~x{M|NU_{?<Wj)cYybdh&Z'
+
+	sock = connect()
+	assert sock, "Connection to server at localhost:2001 failed"
+
+	
+	# Test #1: Attempt registration with unsupported encryption type
+
+	wid = '11111111-1111-1111-1111-222222222222'
+	cmd = ' '.join([ "REGISTER", wid, pwhash, '3DES', devkey, "\r\n" ])
+	print('Bad encryption algorithm\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '309' and parts[1] == 'ENCRYPTION', 'Failed to catch unsupported algorithm'
+
+
+	# Test #2: Send bad WID
+
+	wid = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+	cmd = ' '.join([ "REGISTER", wid, pwhash, algorithm, devkey, "\r\n" ])
+	print('Bad WID\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '400' and parts[1] == 'BAD', 'Failed to catch bad WID'
+
+	sock.send(b'QUIT\r\n')
+
+
+def test_overflow():
+	'''Tests the server's command handling for commands greater than 8K'''
+
+	print("Test: Command Overflow")
+	setup_test()
+
+	wid = '11111111-1111-1111-1111-111111111111'
+	# password is 'SandstoneAgendaTricycle'
+	pwhash = '$argon2id$v=19$m=65536,t=2,p=1$ew5lqHA5z38za+257DmnTA$0LWVrI2r7XCq' \
+				'dcCYkJLok65qussSyhN5TTZP+OTgzEI'
+	algorithm = 'curve25519'
+	devkey = '@X~msiMmBq0nsNnn0%~x{M|NU_{?<Wj)cYybdh&Z'
+
+	sock = connect()
+	assert sock, "Connection to server at localhost:2001 failed"
+
+	wid = 'A' * 10240
+	cmd = ' '.join([ "REGISTER", wid, pwhash, algorithm, devkey, "\r\n" ])
+	print('Overflow\n--------------------------')
+	print('CLIENT: %s' % cmd)
+	sock.send(cmd.encode())
+
+	response = sock.recv(8192).decode()
+	print('SERVER: %s' % response)
+	
+	parts = response.split(' ')
+	assert parts[0] == '400' and parts[1] == 'BAD', 'Failed to catch overflow'
+
+
+	sock.send(b'QUIT\r\n')
 
 
 if __name__ == '__main__':
-	test_register_success()
+	test_register_success_dup()
+	test_register_failures()
+	test_overflow()
