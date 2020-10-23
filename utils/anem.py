adb15e08e8dedd2877e5fff0917547fbf1615e6f
Jon Yoder <jsyoder@mailfence.com>
2020-10-23 06:52:09 -0400

    Initial checkin

---
+++ utils/anem.py
@@ -0,0 +1,5 @@
+# anem.py: Utility to encode and decode the Anselus Encrypted Message format
+# Usage: 	anem encode <keyname> messagefile [attachment1 [attachment2...]]
+#			anem decode file directory
+# ©2020 Jon Yoder <jsyoder@mailfence.com>
+# Released under the MIT license
