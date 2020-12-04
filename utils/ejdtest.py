f65cc5394b5c744f71ce354bc59b3db67f9452c1
Jon Yoder <jsyoder@mailfence.com>
2020-12-04 16:55:38 -0500

    Add hash checking and pass data as a dict

--- utils/ejdtest.py
+++ utils/ejdtest.py
@@ -5,21 +5,32 @@
 # Released under the terms of the MIT license
 # ©2020 Jon Yoder <jsyoder@mailfence.com>
 
-from base64 import b85decode, b85encode
+from base64 import b85encode
+import hashlib
 
 import nacl.public
 import nacl.secret
 import nacl.utils
 from pyanselus.keycard import EncodedString, Base85Encoder
 
-def TestDecrypt(privkeystr : EncodedString, secretkeystr : EncodedString, indata : bytes):
+def TestDecrypt(pubkeystr : EncodedString, privkeystr : EncodedString, indata : dict):
 	'''Test decryption'''
 	
+	secretkeystr = EncodedString(indata['Item']['Key'])
+
 	print("DECRYPTION:")
+	print(f"Public key: {pubkeystr}")
 	print(f"Private key: {privkeystr}")
 	print(f"Encrypted secret key: {secretkeystr}")
 	print(f"Incoming data: {indata}")
 
+	# Hash supplied pubkey and compare to KeyHash
+	hasher = hashlib.blake2b(digest_size=32)
+	hasher.update(pubkeystr.as_string().encode())
+	if indata['Item']['KeyHash'] != "BLAKE2B-256:" + b85encode(hasher.digest()).decode():
+		print("Public key supplied doesn't match key used for file. Unable to decrypt.")
+		return
+	
 	# Decrypt secret key and decode nonce
 	sealedbox = nacl.public.SealedBox(nacl.public.PrivateKey(privkey.raw_data()))
 
@@ -30,10 +41,11 @@ def TestDecrypt(privkeystr : EncodedString, secretkeystr : EncodedString, indata
 		return
 	
 	secretbox = nacl.secret.SecretBox(decryptedkey)
-	secretbox.decrypt(indata)
+	decrypted_data = secretbox.decrypt(indata['Payload'])
+	print(decrypted_data.decode())
 
 
-def TestEncrypt(pubkeystr : EncodedString, indata : bytes) -> (str, bytes):
+def TestEncrypt(pubkeystr : EncodedString, indata : dict) -> dict:
 	'''Test encryption'''
 	
 	# Generate a random secret key and encrypt the data given to us
@@ -41,7 +53,13 @@ def TestEncrypt(pubkeystr : EncodedString, indata : bytes) -> (str, bytes):
 	mynonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
 	secretbox = nacl.secret.SecretBox(secretkey)
 	encrypted_data = secretbox.encrypt(indata, nonce=mynonce)
-	
+
+	# Generate a hash of the public key passed to the function to enable key identification
+	# NOTE: this is a hash of the encoded string -- the prefix, separator, and Base85-encoded key
+	hasher = hashlib.blake2b(digest_size=32)
+	hasher.update(pubkeystr.as_string().encode())
+	keyhash = "BLAKE2B-256:" + b85encode(hasher.digest()).decode()
+
 	# Encrypt the secret key with the public key passed to the function
 	sealedbox = nacl.public.SealedBox(nacl.public.PublicKey(pubkeystr.raw_data()))
 	encryptedkey = 'XSALSA20:' + sealedbox.encrypt(secretkey, Base85Encoder).decode()
@@ -54,9 +72,18 @@ def TestEncrypt(pubkeystr : EncodedString, indata : bytes) -> (str, bytes):
 
 	encryptedkeystr = EncodedString(encryptedkey)
 	if not encryptedkeystr.is_valid():
-		return (None, None)
+		return None
 	
-	return (encryptedkeystr, encrypted_data)
+	outdata = {
+		'Item' : {
+			'Version' : '1.0',
+			'KeyHash' : keyhash,
+			'Key' : encryptedkeystr.as_string(),
+		},
+		'Payload' : encrypted_data
+	}
+
+	return outdata
 
 
 			
@@ -65,4 +92,4 @@ if __name__ == '__main__':
 	privkey = EncodedString(r"CURVE25519:7>4ui(`dvGc1}N!EerhNHk0tY`f-joG25Gd81lcw")
 	data = b'One if by sea, two if by land.'
 	encdata = TestEncrypt(pubkey, data)
-	TestDecrypt(privkey, encdata[0], encdata[1])
+	TestDecrypt(pubkey, privkey, encdata)
