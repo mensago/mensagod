700c3f018c0cc4051a2e14c706aa2441a56824ad
Jon Yoder <jsyoder@mailfence.com>
2020-12-04 16:43:12 -0500

    Introduce using EncodedStrings

--- utils/ejdtest.py
+++ utils/ejdtest.py
@@ -12,41 +12,57 @@ import nacl.secret
 import nacl.utils
 from pyanselus.keycard import EncodedString, Base85Encoder
 
-def TestDecrypt(privkeystr : str, secretkeystr : str, indata : bytes):
+def TestDecrypt(privkeystr : EncodedString, secretkeystr : EncodedString, indata : bytes):
 	'''Test decryption'''
-	secretbox = nacl.secret.SecretBox(secretkeystr)
+	
+	print("DECRYPTION:")
+	print(f"Private key: {privkeystr}")
+	print(f"Encrypted secret key: {secretkeystr}")
+	print(f"Incoming data: {indata}")
+
+	# Decrypt secret key and decode nonce
+	sealedbox = nacl.public.SealedBox(nacl.public.PrivateKey(privkey.raw_data()))
+
+	try:
+		decryptedkey = sealedbox.decrypt(secretkeystr.raw_data())
+	except:
+		print(f"Unable to decrypt the secret key.")
+		return
+	
+	secretbox = nacl.secret.SecretBox(decryptedkey)
 	secretbox.decrypt(indata)
 
 
-def TestEncrypt(pubkeystr : str, indata : bytes) -> (bytes, bytes):
+def TestEncrypt(pubkeystr : EncodedString, indata : bytes) -> (str, bytes):
 	'''Test encryption'''
 	
-	print(f"Public key: {pubkey}")
-	print(f"Incoming data: {indata}")
-
 	# Generate a random secret key and encrypt the data given to us
 	secretkey = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
 	mynonce = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
 	secretbox = nacl.secret.SecretBox(secretkey)
 	encrypted_data = secretbox.encrypt(indata, nonce=mynonce)
-	print(f"Encrypted indata: {encrypted_data}")
 	
 	# Encrypt the secret key with the public key passed to the function
-	sealedbox = nacl.public.SealedBox(nacl.public.PublicKey(b85decode(pubkeystr)))
-	encryptedkey = sealedbox.encrypt(secretkey, Base85Encoder)
+	sealedbox = nacl.public.SealedBox(nacl.public.PublicKey(pubkeystr.raw_data()))
+	encryptedkey = 'XSALSA20:' + sealedbox.encrypt(secretkey, Base85Encoder).decode()
+
+	print("ENCRYPTION:")
+	print(f"Public key: {pubkey}")
+	print(f"Incoming data: {indata}")
 	print(f"Encrypted secret key: {encryptedkey}")
+	print(f"Encrypted indata: {encrypted_data}")
 
-	# Confirm we did it right
-	secretbox.decrypt(encrypted_data)
-
-	return (secretkey, encrypted_data)
+	encryptedkeystr = EncodedString(encryptedkey)
+	if not encryptedkeystr.is_valid():
+		return (None, None)
+	
+	return (encryptedkeystr, encrypted_data)
 
 
 			
 if __name__ == '__main__':
-	pubkey = r"yb8L<$2XqCr5HCY@}}xBPWLHyXZdx&l>+xz%p1*W"
-	privkey = r"7>4ui(`dvGc1}N!EerhNHk0tY`f-joG25Gd81lcw"
-
+	pubkey = EncodedString(r"CURVE25519:yb8L<$2XqCr5HCY@}}xBPWLHyXZdx&l>+xz%p1*W")
+	privkey = EncodedString(r"CURVE25519:7>4ui(`dvGc1}N!EerhNHk0tY`f-joG25Gd81lcw")
 	data = b'One if by sea, two if by land.'
 	encdata = TestEncrypt(pubkey, data)
 	TestDecrypt(privkey, encdata[0], encdata[1])
