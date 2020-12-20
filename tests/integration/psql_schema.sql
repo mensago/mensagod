04b8040f5bb25c789ee67ac782f085fcef20ecea
Jon Yoder <jsyoder@mailfence.com>
2020-12-20 09:39:42 -0500

    Sync schema to setupconfig.py

--- tests/integration/psql_schema.sql
+++ tests/integration/psql_schema.sql
@@ -11,8 +11,8 @@ END $$;
 
 -- Create new ones
 CREATE TABLE iwkspc_main(rowid SERIAL PRIMARY KEY, wid char(36) NOT NULL, 
-	friendly_address VARCHAR(48), password VARCHAR(128) NOT NULL, 
-	status VARCHAR(16) NOT NULL);
+	uid VARCHAR(48), domain VARCHAR(253) NOT NULL, password VARCHAR(128) NOT NULL, 
+	status VARCHAR(16) NOT NULL, type VARCHAR(16) NOT NULL);
 
 CREATE TABLE iwkspc_folders(rowid SERIAL PRIMARY KEY, wid char(36) NOT NULL, 
 	enc_name VARCHAR(128) NOT NULL, enc_key VARCHAR(64) NOT NULL);
@@ -27,3 +27,11 @@ CREATE TABLE failure_log(rowid SERIAL PRIMARY KEY, type VARCHAR(16) NOT NULL,
 
 CREATE TABLE prereg(rowid SERIAL PRIMARY KEY, wid VARCHAR(36) NOT NULL UNIQUE,
 	uid VARCHAR(128) NOT NULL, regcode VARCHAR(128));
+
+CREATE TABLE keycards(rowid SERIAL PRIMARY KEY, owner VARCHAR(64) NOT NULL,
+	creationtime TIMESTAMP NOT NULL, index INTEGER NOT NULL,
+	entry VARCHAR(8192) NOT NULL, fingerprint VARCHAR(96) NOT NULL);
+
+CREATE TABLE orgkeys(rowid SERIAL PRIMARY KEY, creationtime TIMESTAMP NOT NULL, 
+	pubkey VARCHAR(7000), privkey VARCHAR(7000) NOT NULL, 
+	purpose VARCHAR(8) NOT NULL, fingerprint VARCHAR(96) NOT NULL);
