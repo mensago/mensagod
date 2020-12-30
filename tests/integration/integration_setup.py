305cb64f5a622c26462ac98507175ef9cbac5bef
Jon Yoder <jsyoder@mailfence.com>
2020-12-30 12:01:50 -0500

    Begin adding proper test data to db for integration tests

--- tests/integration/integration_setup.py
+++ tests/integration/integration_setup.py
@@ -4,11 +4,52 @@ import os.path
 import re
 import socket
 import sys
+import time
 
 import jsonschema
 import nacl.secret
 import psycopg2
 import toml
+
+from pyanselus.cryptostring import CryptoString
+import pyanselus.keycard as keycard
+
+# Keys used in the various tests. 
+# THESE KEYS ARE STORED ON GITHUB! DO NOT USE THESE FOR ANYTHING EXCEPT UNIT TESTS!!
+
+# Test Organization Information
+
+# Name: Example.com
+# Contact-Admin: ae406c5e-2673-4d3e-af20-91325d9623ca/acme.com
+# Support and Abuse accounts are forwarded to Admin
+# Language: en
+
+# Initial Organization Primary Signing Key: {UNQmjYhz<(-ikOBYoEQpXPt<irxUF*nq25PoW=_
+# Initial Organization Primary Verification Key: r#r*RiXIN-0n)BzP3bv`LA&t4LFEQNF0Q@$N~RF*
+# Initial Organization Primary Verification Key Hash: 
+# 	BLAKE2B-256:ag29av@TUvh-V5KaB2l}H=m?|w`}dvkS1S1&{cMo
+
+# Initial Organization Encryption Key: SNhj2K`hgBd8>G>lW$!pXiM7S-B!Fbd9jT2&{{Az
+# Initial Organization Encryption Key Hash: BLAKE2B-256:-Zz4O7J;m#-rB)2llQ*xTHjtblwm&kruUVa_v(&W
+# Initial Organization Decryption Key: WSHgOhi+bg=<bO^4UoJGF-z9`+TBN{ds?7RZ;w3o
+
+# Test User Information
+
+# Name: Corbin Simons
+# Workspace-ID: 4418bf6c-000b-4bb3-8111-316e72030468
+# Domain: example.com
+
+# Initial User Signing Key: p;XXU0XF#UO^}vKbC-wS(#5W6=OEIFmR2z`rS1j+
+# Initial User Verification Key: 6|HBWrxMY6-?r&Sm)_^PLPerpqOj#b&x#N_#C3}p
+
+# Initial User Contact Request Signing Key: ip52{ps^jH)t$k-9bc_RzkegpIW?}FFe~BX&<V}9
+# Initial User Contact Request Verification Key: d0-oQb;{QxwnO{=!|^62+E=UYk2Y3mr2?XKScF4D
+
+# Initial User Contact Request Encryption Key: j(IBzX*F%OZF;g77O8jrVjM1a`Y<6-ehe{S;{gph
+# Initial User Contact Request Decryption Key: 55t6A0y%S?{7c47p(R@C*X#at9Y`q5(Rc#YBS;r}
+
+# Initial User Primary Encryption Key: nSRso=K(WF{P+4x5S*5?Da-rseY-^>S8VN#v+)IN
+# Initial User Primary Decryption Key: 4A!nTPZSVD#tm78d=-?1OIQ43{ipSpE;@il{lYkg
 
 def load_db_config(config_file_path: str) -> dict:
 	'''Loads the Anselus server configuration from the config file'''
@@ -82,8 +123,98 @@ def setup_test():
 
 	schema_path = os.path.abspath(__file__ + '/../')
 	schema_path = os.path.join(schema_path, 'psql_schema.sql')
-	conn = db_setup(config, schema_path)
-	return conn
+
+	return db_setup(config, schema_path)
+
+
+def config_server(dbconn):
+	'''Adds basic data to the database as if setupconfig had been run.'''
+	
+	# Start off by generating the org's root keycard entry and add to the database
+
+	cur = dbconn.cursor()
+	card = keycard.Keycard()
+	
+	root_entry = keycard.OrgEntry()
+	root_entry.set_fields({
+		'Name':'Acme Widgets, Inc.',
+		'Contact-Admin':'c590b44c-798d-4055-8d72-725a7942f3f6/acme.com',
+		'Language':'en',
+		'Domain':'acme.com',
+		'Primary-Verification-Key':'ED25519:r#r*RiXIN-0n)BzP3bv`LA&t4LFEQNF0Q@$N~RF*',
+		'Encryption-Key':'CURVE25519:SNhj2K`hgBd8>G>lW$!pXiM7S-B!Fbd9jT2&{{Az'
+	})
+
+	initial_ovkey = CryptoString(r'ED25519:r#r*RiXIN-0n)BzP3bv`LA&t4LFEQNF0Q@$N~RF*')
+	initial_oskey = CryptoString(r'ED25519:{UNQmjYhz<(-ikOBYoEQpXPt<irxUF*nq25PoW=_')
+	initial_ovhash = CryptoString(r'BLAKE2B-256:ag29av@TUvh-V5KaB2l}H=m?|w`}dvkS1S1&{cMo')
+
+	initial_epubkey = CryptoString(r'CURVE25519:SNhj2K`hgBd8>G>lW$!pXiM7S-B!Fbd9jT2&{{Az')
+	initial_eprivkey = CryptoString(r'CURVE25519:WSHgOhi+bg=<bO^4UoJGF-z9`+TBN{ds?7RZ;w3o')
+	initial_epubhash = CryptoString(r'BLAKE2B-256:-Zz4O7J;m#-rB)2llQ*xTHjtblwm&kruUVa_v(&W')
+	
+	# Organization sign, hash, and verify
+
+	rv = root_entry.sign(initial_oskey, 'Organization')
+	assert not rv.error(), 'Unexpected RetVal error %s' % rv.error()
+	assert root_entry.signatures['Organization'], 'entry failed to user sign'
+
+	rv = root_entry.generate_hash('BLAKE2B-256')
+	assert not rv.error(), 'entry failed to hash'
+
+	rv = root_entry.verify_signature(initial_ovkey, 'Organization')
+	assert not rv.error(), 'org entry failed to verify'
+
+	status = root_entry.is_compliant()
+	assert not status.error(), f"OrgEntry wasn't compliant: {str(status)}"
+
+	card.entries.append(root_entry)
+	cur.execute("INSERT INTO keycards(owner,creationtime,index,entry,fingerprint) " \
+		"VALUES('organization',%s,%s,%s,%s);",
+		(root_entry.fields['Timestamp'],root_entry.fields['Index'],
+			root_entry.make_bytestring(-1).decode(), root_entry.hash))
+
+	cur.execute("INSERT INTO orgkeys(creationtime, pubkey, privkey, purpose, fingerprint) "
+				"VALUES(%s,%s,%s,'encrypt',%s);",
+				root_entry.fields['Timestamp'], initial_epubkey.as_string(),
+				initial_eprivkey.as_string(), initial_epubhash.as_string())
+
+	cur.execute("INSERT INTO orgkeys(creationtime, pubkey, privkey, purpose, fingerprint) "
+				"VALUES(%s,%s,%s,'sign',%s);",
+				root_entry.fields['Timestamp'], initial_ovkey.as_string(),
+				initial_oskey.as_string(), initial_ovhash.as_string())
+
+	# Sleep for 1 second in order for the new entry's timestamp to be useful
+	time.sleep(1)
+
+	# Chain a new entry to the root
+
+	status = card.chain(initial_oskey, True)
+	assert not status.error(), f'keycard chain failed: {status}'
+	
+	new_entry = status['entry']
+	new_entry.prev_hash = root_entry.hash
+	new_entry.generate_hash('BLAKE2B-256')
+	assert not status.error(), f'chained entry failed to hash: {status}'
+	
+	status = card.verify()
+	assert not status.error(), f'keycard failed to verify: {status}'
+
+	cur.execute("INSERT INTO keycards(owner,creationtime,index,entry,fingerprint) " \
+		"VALUES('organization',%s,%s,%s,%s);",
+		(new_entry.fields['Timestamp'],new_entry.fields['Index'],
+			new_entry.makebytestring(-1).decode(), new_entry.hash))
+
+	# TODO: dump new keys into the key table
+
+
+	# Prereg the admin account
+	admin_wid = 'ae406c5e-2673-4d3e-af20-91325d9623ca'
+	regcode = 'CringeUsherArmbandCoffee'
+	cur.execute(f"INSERT INTO prereg(wid, uid, regcode) VALUES('{admin_wid}', 'admin', '{regcode}');")
+
+	cur.close()
+	dbconn.commit()	
 
 
 def add_workspace(account: dict, dbconn):
