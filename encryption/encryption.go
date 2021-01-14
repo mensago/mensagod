3a092acf8d98d65581ab2d25a2125f0bdd7a29a9
Jon Yoder <jsyoder@mailfence.com>
2021-01-14 17:57:55 -0500

    Add VerifyKey class

--- encryption/encryption.go
+++ encryption/encryption.go
@@ -1,11 +1,80 @@
 package encryption
 
+import (
+	"crypto/ed25519"
+	"errors"
+
+	"github.com/darkwyrm/anselusd/cryptostring"
+)
+
 // This module creates some classes which make working with Twisted Edwards Curve encryption
 // a lot less difficult/confusing
 
 // CryptoKey is a baseline interface to the different kinds of keys defined in this module
 type CryptoKey interface {
-	GetID() string
 	GetEncryptionType() string
 	GetType() string
+}
+
+// VerificationKey is an object to represent just a verification key, not a key pair
+type VerificationKey struct {
+	PublicHash     string
+	PrivateHash    string
+	encryptionType string
+	keyType        string
+	key            cryptostring.CryptoString
+}
+
+// NewVerificationKey creates a new verification key from a CryptoString
+func NewVerificationKey(key cryptostring.CryptoString) *VerificationKey {
+	var newkey VerificationKey
+	if newkey.Set(key) != nil {
+		return nil
+	}
+
+	return &newkey
+}
+
+// GetEncryptionType returns the algorithm used by the key
+func (vkey VerificationKey) GetEncryptionType() string {
+	return vkey.encryptionType
+}
+
+// GetType returns the type of key -- asymmetric or symmetric
+func (vkey VerificationKey) GetType() string {
+	return vkey.keyType
+}
+
+// Verify uses the internal verification key with the passed data and signature and returns true
+// if the signature has verified the data with that key.
+func (vkey VerificationKey) Verify(data []byte, signature cryptostring.CryptoString) (bool, error) {
+	if !signature.IsValid() {
+		return false, errors.New("invalid signature")
+	}
+
+	if signature.Prefix != "ED25519" {
+		return false, errors.New("signature uses unsupported signing algorithm")
+	}
+	digest := signature.RawData()
+	if digest == nil {
+		return false, errors.New("decoding error in signature")
+	}
+
+	verifyKeyDecoded := vkey.key.RawData()
+	if verifyKeyDecoded == nil {
+		return false, errors.New("decoding error in verification key")
+	}
+
+	verifyStatus := ed25519.Verify(verifyKeyDecoded, data, digest)
+
+	return verifyStatus, nil
+}
+
+// Set assigns a CryptoString value to the key
+func (vkey *VerificationKey) Set(key cryptostring.CryptoString) error {
+	if key.Prefix != "ED25519" {
+		return errors.New("unsupported signing algorithm")
+	}
+	vkey.key = key
+	return nil
 }
