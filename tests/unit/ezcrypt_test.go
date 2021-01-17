cec0a6f9a5db6423857faf2c51f9b87125c91ecb
Jon Yoder <jsyoder@mailfence.com>
2021-01-17 11:04:02 -0500

    Add passing test for Sign/Verify

--- tests/unit/ezcrypt_test.go
+++ tests/unit/ezcrypt_test.go
@@ -1,7 +1,6 @@
 package anselusd
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/darkwyrm/anselusd/cryptostring"
@@ -9,28 +8,12 @@ import (
 )
 
 func TestEZCryptEncryptDecrypt(t *testing.T) {
-	// def test_encryptionpair_encrypt_decrypt():
-	// '''Test the encryption and decryption code for the EncryptionPair class'''
-
-	// public_key = CryptoString(r"CURVE25519:(B2XX5|<+lOSR>_0mQ=KX4o<aOvXe6M`Z5ldINd`")
-	// private_key = CryptoString(r"CURVE25519:(Rj5)mmd1|YqlLCUP0vE;YZ#o;tJxtlAIzmPD7b&")
-	// kp = encryption.EncryptionPair(public_key, private_key)
-
-	// test_data = 'This is some encryption test data'
-	// estatus = kp.encrypt(test_data.encode())
-	// assert not estatus.error(), 'test_encryptionpair_encrypt_decrypt: error encrypting test data'
-
-	// dstatus = kp.decrypt(estatus['data'])
-	// assert not dstatus.error(), 'test_encryptionpair_encrypt_decrypt: error decrypting test data'
-	// assert dstatus['data'].decode() == test_data, 'decoded data mismatch'
-
 	pubkey := cryptostring.New("CURVE25519:(B2XX5|<+lOSR>_0mQ=KX4o<aOvXe6M`Z5ldINd`")
 	privkey := cryptostring.New("CURVE25519:(Rj5)mmd1|YqlLCUP0vE;YZ#o;tJxtlAIzmPD7b&")
 	keypair := ezcrypt.NewEncryptionPair(pubkey, privkey)
 
 	testData := "This is some encryption test data"
 	encryptedData, err := keypair.Encrypt([]byte(testData))
-	fmt.Print(encryptedData)
 	if err != nil || encryptedData == "" {
 		t.Fatal("EncryptedPair.Encrypt() failed")
 	}
@@ -44,4 +27,22 @@ func TestEZCryptEncryptDecrypt(t *testing.T) {
 		t.Fatal("EncryptedPair decrypted data mismatch")
 	}
 
+}
+
+func TestEZCryptSignVerify(t *testing.T) {
+	verkey := cryptostring.New("ED25519:PnY~pK2|;AYO#1Z;B%T$2}E$^kIpL=>>VzfMKsDx")
+	signkey := cryptostring.New("ED25519:{^A@`5N*T%5ybCU%be892x6%*Rb2rnYd=SGeO4jF")
+	keypair := ezcrypt.NewSigningPair(verkey, signkey)
+
+	testData := "This is some signing test data"
+
+	signature, err := keypair.Sign([]byte(testData))
+	if err != nil || !signature.IsValid() {
+		t.Fatal("SigningPair.Sign() failed")
+	}
+
+	verified, err := keypair.Verify([]byte(testData), signature)
+	if err != nil || !verified {
+		t.Fatal("SigningPair.Verify() failed")
+	}
 }
