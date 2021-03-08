c663989fd2a946c432dc568c852c72104b164095
Jon Yoder <jyoder@bcadmin.com>
2021-03-08 13:04:27 -0500

    Add function to create dir for test files

--- tests/integration/test_fscmds.py
+++ tests/integration/test_fscmds.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+import time
+
 from pymensago.serverconn import ServerConnection
 from integration_setup import setup_test, init_server
 
@@ -18,6 +22,23 @@ server_response = {
 	}
 }
 
+def setup_testdir(name) -> str:
+	'''Creates a test folder for holding files'''
+	topdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'testfiles')
+	if not os.path.exists(topdir):
+		os.mkdir(topdir)
+
+	testdir = os.path.join(topdir, name)
+	while os.path.exists(testdir):
+		try:
+			shutil.rmtree(testdir)
+		except:
+			print("Waiting a second for test folder to unlock")
+			time.sleep(1.0)
+	os.mkdir(testdir)
+	return testdir
+
+
 def test_upload():
 	'''Tests the UPLOAD command'''
 
@@ -26,6 +47,19 @@ def test_upload():
 
 	conn = ServerConnection()
 	assert conn.connect('localhost', 2001), "Connection to server at localhost:2001 failed"
+
+	testpath = setup_testdir('test_upload')
+
+	# Make a test file
+	try:
+		fhandle = open(os.path.join(testpath, 'uploadme.txt'), 'w')
+	except Exception as e:
+		assert False, f"test_upload: exception thrown creating temp file: {e}"
+	
+	fhandle.write('0' * 1000)
+	fhandle.close()
+
+	# Subtest #1:
 
 if __name__ == '__main__':
 	test_upload()
