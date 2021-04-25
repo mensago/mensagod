a705fcccbd76257ba252099124788efa39ad865e
Jon Yoder <jsyoder@mailfence.com>
2021-04-25 12:50:40 -0400

    Implement message queueing and shutdown

--- delivery/delivery.go
+++ delivery/delivery.go
@@ -2,7 +2,6 @@ package delivery
 
 import (
 	"container/list"
-	"errors"
 	"sync"
 
 	"github.com/spf13/viper"
@@ -16,39 +15,76 @@ type messageInfo struct {
 
 var messageQueue *list.List
 var queueLock sync.Mutex
+var workerCount int
+var workerGroup sync.WaitGroup
 var maxWorkers int
+var workerLock sync.Mutex
+var quitFlag bool
+var quitLock sync.RWMutex
 
 func Init() {
 	messageQueue = list.New()
 	maxWorkers = viper.GetInt("performance.max_delivery_threads")
 }
 
+func Shutdown() {
+	quitLock.Lock()
+	quitFlag = true
+	quitLock.Unlock()
+	workerGroup.Wait()
+}
+
 // PushMessage enqueues a message for delivery
-func PushMessage(sender string, recipientDomain string, path string) error {
+func PushMessage(sender string, recipientDomain string, path string) {
 	info := messageInfo{Sender: sender, Recipient: recipientDomain, Path: path}
 	queueLock.Lock()
 	messageQueue.PushBack(info)
 	queueLock.Unlock()
 
-	return errors.New("unimplemented")
+	workerLock.Lock()
+	if workerCount < maxWorkers {
+		workerCount++
+		workerGroup.Add(1)
+		go deliveryWorker()
+	}
+	workerLock.Unlock()
 }
 
 // popMessage removes a message from the queue
-func popMessage() (*messageInfo, error) {
+func popMessage() *messageInfo {
 	var out messageInfo
 	queueLock.Lock()
 	defer queueLock.Unlock()
 
 	item := messageQueue.Front()
 	if item == nil {
-		return nil, nil
+		return nil
 	}
 
 	out = item.Value.(messageInfo)
 	messageQueue.Remove(item)
-	return &out, errors.New("unimplemented")
+	return &out
 }
 
 func deliveryWorker() {
+	for {
+		quitLock.RLock()
+		if quitFlag {
+			quitLock.RUnlock()
+			break
+		}
+		quitLock.RUnlock()
 
+		msgInfo := popMessage()
+		if msgInfo == nil {
+			break
+		}
+
+		// Handle delivery here
+	}
+
+	workerLock.Lock()
+	workerCount--
+	workerGroup.Done()
+	workerLock.Unlock()
 }
