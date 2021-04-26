1eba8ca74d1d43f203456b20097698576d2541c9
Jon Yoder <jsyoder@mailfence.com>
2021-04-26 13:12:08 -0400

    Add comments on how delivery will be handled

--- delivery/delivery.go
+++ delivery/delivery.go
@@ -80,7 +80,40 @@ func deliveryWorker() {
 			break
 		}
 
-		// Handle delivery here
+		// Delivery Handling
+		// - get an item from the queue
+		// - check the path and ensure file exists. The internal data structure will have been
+		//		verified when the file was uploaded for delivery
+		// - check the destination domain. Follow the appropriate protocol below based on its
+		//		destination
+
+		// Internal Delivery
+		// - Load file and decrypt recipient information
+		// - Get workspace ID from recipient information and move file to the /new directory for
+		//		the recipient
+		// - Check if workspace has any active client connections. If it does, send an update
+		//		message to the client workers that there is a new message
+
+		// External Delivery
+		// - Connect to external server using SERVERID. TL;DR: sending and receiving servers get
+		//		PEK from each other's DNS record and use random encrypted challenges to mutually
+		//		authenticate.
+
+		//		- Sending server gets receiving server's PEK from its DNS record
+		//		- Sender generates receiver challenge and encrypts it with receiver's PEK
+		//		- Sender issues SERVERID command with receiver's encrypted challenge and desired
+		//			recipient domain
+		// 		- Receiver gets PEK from sending server's DNS record
+		//		- Receiver generates sender challenge and encrypts it with sender's PEK
+		//		- Receiver descrypts its own challenge and attaches it and the sender's encrypted
+		//			challenge to the response
+		//		- Sender confirms the receiver's successful response
+		//		- Sender decrypts the receiver's challenge, attaches the decrypted response, and
+		//			sends it to the receiver
+		//		- Receiver confirm's sender's successful decryption
+		//		- Receiver notifies sender is authenticated. Sender may proceed.
+		// - Sender issues DELIVER command and uploads message
+		// - Receiver decrypts sender information and returns appropriate delivery response code
 	}
 
 	workerLock.Lock()
