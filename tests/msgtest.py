74ad98a6a60f4513195aac3f3d79c7c59107808a
Jon Yoder <jsyoder@mailfence.com>
2021-04-29 16:27:21 -0400

    Start a messaging test script

---
+++ tests/msgtest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pymensago.encryption import 
+
+msgRecipients = {
+	'To' : '22222222-2222-2222-2222-222222222222/example.com'
+}
+
+msgSender = {
+	'Sender' : '11111111-1111-1111-1111-111111111111/example.com'
+}
+
+sampleMsg = {
+	'Type' : 'usermessage',
+	'Version' : '1.0',
+	'RecipientKey' : '',
+	'Recipients' : '',
+	'SenderKey' : ''
+	'Sender' : '',
+	'Date' : '20190905 155323',
+}
