200d2b73a1315001fa92a56ccb31b2eb0866e8b3
Jon Yoder <jsyoder@mailfence.com>
2021-05-06 12:46:47 -0400

    Implement new message notification base functions

--- messaging/messaging.go
+++ messaging/messaging.go
@@ -1,5 +1,12 @@
 package messaging
 
+import (
+	"errors"
+	"sync"
+
+	"github.com/darkwyrm/mensagod/dbhandler"
+)
+
 type SealedEnvelope struct {
 	Type       string
 	Version    string
@@ -50,4 +57,61 @@ type Attachment struct {
 	Name string
 	Type string
 	Data string
+}
+
+// widList is a map of workspace IDs to UNIX timestamps used for update notifications. The
+// timestamp for a workspace is periodically checked by all worker goroutines which handle it. The
+// timestamp is updated by either a delivery goroutine or a worker goroutine when a new message
+// is placed in <wid>/new. Worker goroutines can then notify client devices of new messages.
+var widList map[string]int64
+var widListLock sync.RWMutex
+
+func init() {
+	widList = make(map[string]int64)
+}
+
+// RegisterWorkspace ensures that the workspace update list has a specific workspace ID. If it has
+// already been registered, nothing happens.
+func RegisterWorkspace(wid string) error {
+	if !dbhandler.ValidateUUID(wid) {
+		return errors.New("invalid workspace id")
+	}
+
+	widListLock.Lock()
+	defer widListLock.Unlock()
+
+	_, exists := widList[wid]
+	if !exists {
+		widList[wid] = -1
+	}
+
+	return nil
+}
+
+// Gets the UTC UNIX timestamp of the last new message notification for the workspace. If none
+// has occurred, then -1 in returned.
+func LastWorkspaceUpdate(wid string) int64 {
+	widListLock.RLock()
+	defer widListLock.RUnlock()
+
+	timestamp, exists := widList[wid]
+	if !exists {
+		return -1
+	}
+
+	return timestamp
+}
+
+// UpdateWorkspace updates the timestamp for the last new message for a workspace
+func UpdateWorkspace(wid string, timestamp int64) error {
+	if !dbhandler.ValidateUUID(wid) {
+		return errors.New("invalid workspace id")
+	}
+
+	widListLock.Lock()
+	defer widListLock.Unlock()
+
+	widList[wid] = timestamp
+
+	return nil
 }
