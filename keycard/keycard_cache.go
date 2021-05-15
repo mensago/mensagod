4cf15a734e579a088dd8576d7910bd533e00d086
Jon Yoder <jsyoder@mailfence.com>
2021-05-15 07:47:14 -0400

    Begin implementing a keycard cache skeleton

---
+++ keycard/keycard_cache.go
@@ -0,0 +1,15 @@
+package keycard
+
+import "errors"
+
+func GetKeycard(address string) (*Keycard, error) {
+
+	// TODO: Implement GetKeycard
+	return nil, errors.New("unimplemented")
+}
+
+func ResolveAddress(address string) (string, error) {
+
+	// TODO: Implement ResolveAddress
+	return "", errors.New("unimplemented")
+}
