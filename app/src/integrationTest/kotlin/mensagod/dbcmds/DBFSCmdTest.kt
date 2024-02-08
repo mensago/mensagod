b90e786c497faa66beba2e57aa268fae16fc0121
Jon Yoder <jon@yoder.cloud>
2024-02-08 15:43:49 -0500

    Implement setQuota() add test stub

---
+++ app/src/integrationTest/kotlin/mensagod/dbcmds/DBFSCmdTest.kt
@@ -0,0 +1,11 @@
+package mensagod.dbcmds
+
+import org.junit.jupiter.api.Test
+
+class DBFSCmdTest {
+
+    @Test
+    fun quotaTest() {
+        // TODO: Implement quotaTest()
+    }
+}
--- app/src/main/kotlin/mensagod/dbcmds/Filesystem.kt
+++ app/src/main/kotlin/mensagod/dbcmds/Filesystem.kt
@@ -2,7 +2,10 @@ package mensagod.dbcmds
 
 import libkeycard.RandomID
 import mensagod.DBConn
+import mensagod.MServerPath
 import mensagod.NotConnectedException
+import mensagod.ResourceNotFoundException
+import mensagod.fs.LocalFS
 
 /**
  * Returns a pair of integers representing the current disk usage and quota size.
@@ -45,9 +48,31 @@ fun resetQuotaUsage(db: DBConn): Throwable? {
  * @throws NotConnectedException Returned if not connected to the database
  * @throws java.sql.SQLException Returned for database problems, most likely either with your query
  * or with the connection
+ * @throws ResourceNotFoundException Returned if the workspace doesn't exist
  */
 fun setQuota(db: DBConn, wid: RandomID, quota: Int): Throwable? {
-    TODO("Implement setQuota($db, $wid, $quota)")
+    val rows = db.execute("UPDATE quotas SET quota=? WHERE wid=?", quota, wid)
+        .getOrElse { return it }
+
+    // We made a change, so we're done here
+    if (rows != null) return null
+
+    // No change made, so we need to update things
+    val lfs = LocalFS.get()
+    val userWidPath = MServerPath("/ wsp $wid")
+    val handle = lfs.entry(userWidPath)
+    var usage = 0L
+    if (handle.exists().getOrElse { return it }) {
+        usage = lfs.getDiskUsage(userWidPath).getOrElse { return it }
+    } else {
+        // If the directory for a workspace doesn't exist, check to see if it's at least an
+        // actual workspace somewhere in the system
+        val status = try { checkWorkspace(db, wid) } catch (e: Exception) { return e }
+        if (status == null) return ResourceNotFoundException()
+    }
+
+    return db.execute("INSERT INTO quotas(wid, usge, quota) VALUES(?,?,?)", wid, usage, quota)
+        .exceptionOrNull()
 }
 
 /**
