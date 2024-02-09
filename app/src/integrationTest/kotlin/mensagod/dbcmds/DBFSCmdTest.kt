1a63e31fa7737da2321cc18cd4d3944513402d30
Jon Yoder <jon@yoder.cloud>
2024-02-09 13:07:15 -0500

    Start work on setQuotaTest

--- app/src/integrationTest/kotlin/mensagod/dbcmds/DBFSCmdTest.kt
+++ app/src/integrationTest/kotlin/mensagod/dbcmds/DBFSCmdTest.kt
@@ -1,11 +1,18 @@
 package mensagod.dbcmds
 
+import mensagod.DBConn
+import mensagod.setupTest
+import mensagod.setupUser
 import org.junit.jupiter.api.Test
 
 class DBFSCmdTest {
 
     @Test
-    fun quotaTest() {
-        // TODO: Implement quotaTest()
+    fun setQuotaTest() {
+        val setupData = setupTest("dbcmds.setQuota")
+        val db = DBConn()
+        setupUser(db)
+
+        // TODO: Implement setQuotaTest()
     }
 }
