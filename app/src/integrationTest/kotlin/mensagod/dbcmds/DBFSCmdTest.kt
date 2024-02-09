2f97a45c61c42623f682ab542056ff2d3041de61
Jon Yoder <jon@yoder.cloud>
2024-02-09 15:51:39 -0500

    Implement getQuotaInfo() test

--- app/src/integrationTest/kotlin/mensagod/dbcmds/DBFSCmdTest.kt
+++ app/src/integrationTest/kotlin/mensagod/dbcmds/DBFSCmdTest.kt
@@ -9,6 +9,30 @@ import java.nio.file.Paths
 class DBFSCmdTest {
 
     @Test
+    fun getQuotaInfoTest() {
+        val setupData = setupTest("dbcmds.getQuotaInfo")
+        val db = DBConn()
+        setupUser(db)
+
+        val userWID = RandomID.fromString(USER_PROFILE_DATA["wid"])!!
+        val userTopPath = Paths.get(setupData.testPath, "topdir", "wsp", userWID.toString())
+        userTopPath.toFile().mkdirs()
+        makeTestFile(userTopPath.toString(), fileSize = 1024)
+
+        val info = getQuotaInfo(db, userWID).getOrThrow()
+        val rs = db.query("SELECT usage,quota FROM quotas WHERE wid=?", userWID).getOrThrow()
+        assert(rs.next())
+        assertEquals(info.first, rs.getLong("usage"))
+        assertEquals(info.second, rs.getLong("quota"))
+
+        makeTestFile(userTopPath.toString(), fileSize = 1024)
+
+        // This should be different from the actual value -- we are expected
+        // to notify the system of any changes while the server is running
+        assertEquals(info.first, getQuotaInfo(db, userWID).getOrThrow().first)
+    }
+
+    @Test
     fun setQuotaTest() {
         val setupData = setupTest("dbcmds.setQuota")
         val db = DBConn()
