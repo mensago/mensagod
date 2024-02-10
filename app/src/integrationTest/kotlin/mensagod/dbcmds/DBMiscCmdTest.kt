ab23dd816c2f7beca62f9d2c1b2346e7d34a94ed
Jon Yoder <jon@yoder.cloud>
2024-02-10 12:20:05 -0500

    Start implementing test for addUpdateRecord()

--- app/src/integrationTest/kotlin/mensagod/dbcmds/DBMiscCmdTest.kt
+++ app/src/integrationTest/kotlin/mensagod/dbcmds/DBMiscCmdTest.kt
@@ -1,16 +1,35 @@
 package mensagod.dbcmds
 
-import mensagod.DBConn
-import mensagod.ServerConfig
-import mensagod.initDB
-import mensagod.resetDB
+import libkeycard.RandomID
+import mensagod.*
 import org.junit.jupiter.api.Test
+import java.time.Instant
+import java.util.*
 
 class DBMiscCmdTest {
 
     @Test
     fun addUpdateRecordTest() {
-        // TODO: Implement test for addUpdateRecord()
+        setupTest("dbcmds.addUpdateRecordTest")
+        val db = DBConn()
+        setupUser(db)
+
+        val adminWID = RandomID.fromString(ADMIN_PROFILE_DATA["wid"])!!
+        val devid = RandomID.fromString(ADMIN_PROFILE_DATA["devid"])!!
+        val unixTime = "12345678"
+        val testDirID = RandomID.fromString("77d62128-4f94-432d-9e1b-4f5d6a2bd632")
+        val testFileName =
+            "${Instant.now().epochSecond}.1024.${UUID.randomUUID().toString().lowercase()}"
+
+        val createID = RandomID.fromString("6dce4c07-dfbe-48ff-bc1a-cf3d2c33dd70")!!
+        addUpdateRecord(db, adminWID, UpdateRecord(
+            createID, UpdateType.Create,"/ wsp $adminWID $testFileName", unixTime, devid
+        ))?.let { throw it }
+
+        // Note that one of the errors for Update.Create should be checking that the data points
+        // to a file and not a directory. Creating a directory should be MkDir
+
+        // TODO: Finish implementing test for addUpdateRecord()
     }
 
     @Test
