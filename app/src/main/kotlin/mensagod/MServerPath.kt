729cf3b17c8a058b80b0dd00fa3d54dd49c142dc
Jon Yoder <jon@yoder.cloud>
2024-02-11 12:40:13 -0500

    Add comparison overrides

--- app/src/main/kotlin/mensagod/MServerPath.kt
+++ app/src/main/kotlin/mensagod/MServerPath.kt
@@ -81,6 +81,16 @@ class MServerPath(path: String? = null) {
         return this
     }
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MServerPath
+        return value == other.value
+    }
+
+    override fun hashCode(): Int { return value.hashCode() }
+
     override fun toString(): String { return value }
 
     companion object {
