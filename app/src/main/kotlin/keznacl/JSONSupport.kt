9b2b92df7e8313d4ffe4ca1fe1b7959fdc1e576d
Jon Yoder <jon@yoder.cloud>
2024-02-15 11:05:24 -0500

    Function case fix

--- app/src/main/kotlin/keznacl/JSONSupport.kt
+++ app/src/main/kotlin/keznacl/JSONSupport.kt
@@ -12,7 +12,7 @@ import java.security.GeneralSecurityException
  * specification
  * @throws GeneralSecurityException Returned for errors during the encryption process.
  */
-inline fun <reified T>SerializeAndEncrypt(obj: T, key: Encryptor): Result<CryptoString> {
+inline fun <reified T>serializeAndEncrypt(obj: T, key: Encryptor): Result<CryptoString> {
     val rawJSON = try { Json.encodeToString(obj) }
         catch (e: Exception) { return Result.failure(e) }
     return key.encrypt(rawJSON.encodeToByteArray())
@@ -26,7 +26,7 @@ inline fun <reified T>SerializeAndEncrypt(obj: T, key: Encryptor): Result<Crypto
  * specification
  * @throws GeneralSecurityException Returned for errors during the encryption process.
  */
-inline fun <reified T>DecryptAndDeserialize(cs: CryptoString, key: EncryptionPair): Result<T> {
+inline fun <reified T>decryptAndDeserialize(cs: CryptoString, key: EncryptionPair): Result<T> {
     val rawJSON = key.decrypt(cs).getOrElse { return Result.failure(it) }.decodeToString()
     val out = try { Json.decodeFromString<T>(rawJSON) }
         catch (e: Exception) { return Result.failure(e) }
