12167f1b9dfb5c458b2c90041add9734abd928fa
Jon Yoder <jon@yoder.cloud>
2024-02-15 10:26:42 -0500

    Add Message class, which we'll need in short order.

---
+++ app/src/main/kotlin/mensagod/messaging/Message.kt
@@ -0,0 +1,44 @@
+package mensagod.messaging
+
+import keznacl.Base85
+import libkeycard.RandomID
+import libkeycard.Timestamp
+import libkeycard.WAddress
+import java.security.SecureRandom
+
+/**
+ * Class which represents a Mensago message. It is not as full-featured as a class for a Mensago
+ * client would be, as the only type of messages that a server will generate would be system
+ * messages, so there is no need for support for attachments and other user-focused features.
+ */
+class Message(var from: WAddress, var to: WAddress) {
+
+    var version = 1.0f
+    var type = "system"
+    var subtype = ""
+    var id = RandomID.generate()
+    var threadID = RandomID.generate()
+    var format = "text"
+
+    var date: Timestamp = Timestamp()
+    var subject = ""
+    var body = ""
+    var randomString: String = makePadding(1..24)
+
+    companion object {
+
+        /** Private method just for making padding strings of random characters */
+        private fun makePadding(range: IntRange? = null): String {
+            val actualRange = range ?: 1..24
+
+            val rng = SecureRandom()
+            val size = rng.nextInt(actualRange.first, actualRange.last + 1)
+            return Base85.rfc1924Encoder.encode(
+                ByteArray(size) { rng.nextInt(0, 256).toByte() }
+            )
+        }
+
+    }
+}
+
+
