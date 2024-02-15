514ec220abb1a7fc0ab973274eac6c3eda197b89
Jon Yoder <jon@yoder.cloud>
2024-02-15 10:59:29 -0500

    Replace encrypt method for info classes with templatized implementation in keznacl

---
+++ app/src/main/kotlin/keznacl/JSONSupport.kt
@@ -0,0 +1,34 @@
+package keznacl
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.security.GeneralSecurityException
+
+/**
+ * Serializes an object to JSON and returns the encrypted version of it.
+ *
+ * @throws kotlinx.serialization.SerializationException Returned for encoding-specific errors
+ * @throws IllegalArgumentException Returned if the encoded input does not comply format's
+ * specification
+ * @throws GeneralSecurityException Returned for errors during the encryption process.
+ */
+inline fun <reified T>SerializeAndEncrypt(obj: T, key: Encryptor): Result<CryptoString> {
+    val rawJSON = try { Json.encodeToString(obj) }
+        catch (e: Exception) { return Result.failure(e) }
+    return key.encrypt(rawJSON.encodeToByteArray())
+}
+
+/**
+ * Serializes an object to JSON and returns the encrypted version of it.
+ *
+ * @throws kotlinx.serialization.SerializationException Returned for encoding-specific errors
+ * @throws IllegalArgumentException Returned if the encoded input does not comply format's
+ * specification
+ * @throws GeneralSecurityException Returned for errors during the encryption process.
+ */
+inline fun <reified T>DecryptAndDeserialize(cs: CryptoString, key: EncryptionPair): Result<T> {
+    val rawJSON = key.decrypt(cs).getOrElse { return Result.failure(it) }.decodeToString()
+    val out = try { Json.decodeFromString<T>(rawJSON) }
+        catch (e: Exception) { return Result.failure(e) }
+    return Result.success(out)
+}
--- app/src/main/kotlin/mensagod/messaging/MsgTypes.kt
+++ app/src/main/kotlin/mensagod/messaging/MsgTypes.kt
@@ -1,55 +1,21 @@
 package mensagod.messaging
 
 import keznacl.CryptoString
-import keznacl.Encryptor
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import libkeycard.Domain
 import libkeycard.Timestamp
 import libkeycard.WAddress
-import java.security.GeneralSecurityException
 
 /**
  * The RecipientInfo class contains the information used by the receiving domain's server to
  * deliver a message to its recipient.
  */
-class RecipientInfo(var to: WAddress, var senderDom: Domain) {
-
-    /**
-     * Serializes the object to JSON and returns the encrypted version
-     *
-     * @throws kotlinx.serialization.SerializationException Returned for encoding-specific errors
-     * @throws IllegalArgumentException Returned if the encoded input does not comply format's
-     * specification
-     * @throws GeneralSecurityException Returned for errors during the encryption process.
-     */
-    fun encrypt(key: Encryptor): Result<CryptoString> {
-        val rawJSON = try { Json.encodeToString(this) }
-            catch(e: Exception) { return Result.failure(e) }
-        return key.encrypt(rawJSON.encodeToByteArray())
-    }
-}
+class RecipientInfo(var to: WAddress, var senderDom: Domain)
 
 /**
  * The SenderInfo class contains the information used by the sending domain's server to
  * deliver a message to the server for its recipient.
  */
-class SenderInfo(var from: WAddress, var recipientDom: Domain) {
-
-    /**
-     * Serializes the object to JSON and returns the encrypted version
-     *
-     * @throws kotlinx.serialization.SerializationException Returned for encoding-specific errors
-     * @throws IllegalArgumentException Returned if the encoded input does not comply format's
-     * specification
-     * @throws GeneralSecurityException Returned for errors during the encryption process.
-     */
-    fun encrypt(key: Encryptor): Result<CryptoString> {
-        val rawJSON = try { Json.encodeToString(this) }
-        catch(e: Exception) { return Result.failure(e) }
-        return key.encrypt(rawJSON.encodeToByteArray())
-    }
-}
+class SenderInfo(var from: WAddress, var recipientDom: Domain)
 
 /**
  * The Envelope is a data structure class which represents the public delivery information for a
