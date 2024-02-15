b4b27e2510a69c3bc295b9796d28103194a1c727
Jon Yoder <jon@yoder.cloud>
2024-02-15 10:36:16 -0500

    Implement RecipientInfo::encrypt()

--- app/src/main/kotlin/mensagod/messaging/MsgTypes.kt
+++ app/src/main/kotlin/mensagod/messaging/MsgTypes.kt
@@ -1,16 +1,34 @@
 package mensagod.messaging
 
 import keznacl.CryptoString
+import keznacl.Encryptor
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import libkeycard.Domain
-import libkeycard.RandomID
 import libkeycard.Timestamp
 import libkeycard.WAddress
+import java.security.GeneralSecurityException
 
 /**
  * The RecipientInfo class contains the information used by the receiving domain's server to
  * deliver a message to its recipient.
  */
-class RecipientInfo(var to: WAddress, var senderDom: Domain)
+class RecipientInfo(var to: WAddress, var senderDom: Domain) {
+
+    /**
+     * Serializes the object to JSON and returns the encrypted version
+     *
+     * @throws kotlinx.serialization.SerializationException Returned for encoding-specific errors
+     * @throws IllegalArgumentException Returned if the encoded input does not comply format's
+     * specification
+     * @throws GeneralSecurityException Returned for errors during the encryption process.
+     */
+    fun encrypt(key: Encryptor): Result<CryptoString> {
+        val rawJSON = try { Json.encodeToString(this) }
+            catch(e: Exception) { return Result.failure(e) }
+        return key.encrypt(rawJSON.encodeToByteArray())
+    }
+}
 
 /**
  * The SenderInfo class contains the information used by the sending domain's server to
@@ -24,12 +42,3 @@ class SenderInfo(var from: WAddress, var recipientDom: Domain)
  */
 class Envelope(var type: String, var version: String, var receiver: RecipientInfo,
                var sender: SenderInfo, var date: Timestamp, var payloadKey: CryptoString)
-
-/**
- * The Attachment class holds the data used for file attachments to messages and other data models
- */
-class Attachment(var name: String, var type: String, var data: String)
-
-/** The MsgBody class is a model for unencrypted messages */
-class MsgBody(var from: String, var to: String, var date: Timestamp, var threadID: RandomID,
-              var subject: String, var body: String, var attachments: MutableList<Attachment>)
