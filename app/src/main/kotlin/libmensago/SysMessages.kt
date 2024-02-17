e6c6808f677c4e3f488357ef6d46e4075fa1d3d6
Jon Yoder <jon@yoder.cloud>
2024-02-17 17:56:21 -0500

    DeviceApprovalMsg constructor

--- app/src/main/kotlin/libmensago/SysMessages.kt
+++ app/src/main/kotlin/libmensago/SysMessages.kt
@@ -1,31 +1,27 @@
 package libmensago
 
+import keznacl.CryptoString
 import libkeycard.Timestamp
 import libkeycard.WAddress
 import java.net.InetAddress
+import javax.activation.MimeType
 
 /**
  * The DeviceApprovalMsg class represents a device approval message.
  */
-class DeviceApprovalMsg private constructor(from: WAddress, to: WAddress):
+class DeviceApprovalMsg(from: WAddress, to: WAddress, addr: InetAddress, devInfo: CryptoString):
     Message(from, to, MsgFormat.Text) {
 
     init {
         type = MsgType.System
         subType = "devrequest"
-    }
-    companion object {
-        /**
-         * Creates a new device approval message.
-         */
-        fun new(from: WAddress, to: WAddress, addr: InetAddress): Result<DeviceApprovalMsg> {
-            val out = DeviceApprovalMsg(from, to)
-            out.setSubject("New Device Login")
+        subject = "New Device Login"
 
-            val ts = Timestamp()
-            // TODO: Implement DeviceApprovalMsg::new()
-
-            return Result.success(out)
-        }
+        val ts = Timestamp()
+        body = "Timestamp:$ts\r\nIP-Address:$addr\r\n"
+        attach(Attachment("deviceinfo",
+            MimeType("application/vnd.mensago.encrypted-json"),
+            devInfo.toString().encodeToByteArray()
+        ))
     }
 }
