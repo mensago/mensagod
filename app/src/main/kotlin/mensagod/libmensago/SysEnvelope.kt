53bedd6f73e1afb5f8130cee0b4517d1862d508c
Jon Yoder <jon@yoder.cloud>
2024-02-17 09:42:45 -0500

    Fix accidental breakage of SysEnvelope

--- app/src/main/kotlin/mensagod/libmensago/SysEnvelope.kt
+++ app/src/main/kotlin/mensagod/libmensago/SysEnvelope.kt
@@ -2,12 +2,15 @@ package mensagod.libmensago
 
 import keznacl.CryptoString
 import keznacl.Encryptor
+import keznacl.SecretKey
 
 /**
  * This contains the encrypted header information for a Mensago system message.
  */
-class SysEnvelope(val subtype: String, receiver: RecipientInfo, sender: SenderInfo,
-                  payloadKey: CryptoString): Envelope(receiver, sender, payloadKey) {
+class SysEnvelope private constructor (val subtype: String, receiver: RecipientInfo,
+                                       sender: SenderInfo, payloadKey: CryptoString,
+                                       keyHash: CryptoString, message: Message):
+    Envelope(receiver, sender, payloadKey, keyHash, message) {
 
     init {
         type = MsgType.System
@@ -18,5 +21,12 @@ class SysEnvelope(val subtype: String, receiver: RecipientInfo, sender: SenderIn
      */
     fun seal(senderKey: Encryptor, receiverKey: Encryptor): Result<SealedSysEnvelope> {
         TODO("Implement SysEnvelope::seal($senderKey, $receiverKey)")
+    }
+
+    companion object {
+        fun new(receiver: RecipientInfo, sender: SenderInfo, payloadKey: SecretKey):
+                Result<Envelope> {
+            TODO("Implement SysEnvelope::new($receiver, $sender, $payloadKey)")
+        }
     }
 }
