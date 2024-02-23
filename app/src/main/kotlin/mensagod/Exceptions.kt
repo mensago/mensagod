1582039484308e79fef1bbdc1b4c239560484160
Jon Yoder <jon@yoder.cloud>
2024-02-22 19:23:55 -0500

    Fix regression from last commit

--- app/src/main/kotlin/mensagod/Exceptions.kt
+++ app/src/main/kotlin/mensagod/Exceptions.kt
@@ -1,4 +1,5 @@
 package mensagod
 
-class DatabaseCorruptionException(message: String = ""): Exception(message)
-class FSFailureException(message: String = ""): Exception(message)
+class CancelException(message: String = "") : Exception(message)
+class DatabaseCorruptionException(message: String = "") : Exception(message)
+class FSFailureException(message: String = "") : Exception(message)
