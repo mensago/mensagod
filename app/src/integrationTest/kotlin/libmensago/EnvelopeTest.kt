165b3e8192a4e41128f986cd6e30136daf9f04fd
Jon Yoder <jon@yoder.cloud>
2024-02-27 15:14:01 -0500

    Continue implementing sealOpenTest()

--- app/src/integrationTest/kotlin/libmensago/EnvelopeTest.kt
+++ app/src/integrationTest/kotlin/libmensago/EnvelopeTest.kt
@@ -1,11 +1,14 @@
 package libmensago
 
-import keznacl.EncryptionKey
+import keznacl.EncryptionPair
 import libkeycard.WAddress
+import mensagod.DBConn
+import mensagod.dbcmds.getEncryptionPair
 import org.junit.jupiter.api.Test
 import testsupport.ADMIN_PROFILE_DATA
 import testsupport.FakeDNSHandler
 import testsupport.setupTest
+import java.io.File
 import java.time.Instant
 import java.util.*
 
@@ -23,15 +26,28 @@ class EnvelopeTest {
             .setSubject("Test Message")
             .setBody("This message is just a test")
 
-        val adminKey = EncryptionKey.fromString(ADMIN_PROFILE_DATA["encryption.public"]!!)
+        val adminPair = EncryptionPair.fromStrings(
+            ADMIN_PROFILE_DATA["encryption.public"]!!,
+            ADMIN_PROFILE_DATA["encryption.private"]!!
+        )
             .getOrThrow()
-        val sealed = Envelope.seal(adminKey, msg, FakeDNSHandler()).getOrThrow()
+        val fakeDNS = FakeDNSHandler()
+        val sealed = Envelope.seal(adminPair, msg, fakeDNS).getOrThrow()
 
-        val filelength = sealed.toString().length
-        val filename =
+        val filelength = sealed.toStringResult().getOrThrow().length
+        val filePath = listOf(
+            setupData.testPath,
             "${Instant.now().epochSecond}.$filelength.${UUID.randomUUID().toString().lowercase()}"
-        sealed.saveFile(listOf(setupData.testPath, filename).toPath().toString())
+        ).toPath().toString()
+        sealed.saveFile(filePath)?.let { throw it }
 
+        val loaded = Envelope.loadFile(File(filePath)).getOrThrow()
+        val epair = getEncryptionPair(DBConn()).getOrThrow()
+
+        loaded.tag.decryptSender(epair).getOrThrow()
+        loaded.tag.decryptReceiver(epair).getOrThrow()
+
+        loaded.open(adminPair).getOrThrow()
 
         // TODO: Implement sealOpenTest
     }
