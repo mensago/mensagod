a3d78c685ae85b724976e88b2811782113e5cd67
Jon Yoder <jon@yoder.cloud>
2024-02-26 19:30:38 -0500

    Add another convenience function to convert lists of Strings to a Path

--- app/src/main/kotlin/libmensago/Extensions.kt
+++ app/src/main/kotlin/libmensago/Extensions.kt
@@ -1,9 +1,19 @@
 package libmensago
 
+import java.nio.file.Path
+import java.nio.file.Paths
+
 inline fun <reified T> T.toSuccess(): Result<T> {
     return Result.success(this)
 }
 
 inline fun <reified T> Throwable.toFailure(): Result<T> {
     return Result.failure(this)
+}
+
+fun List<String>.toPath(): Path {
+    return if (this.size == 1)
+        Paths.get(this[0])
+    else
+        Paths.get(this[0], *this.subList(1, this.size).toTypedArray())
 }
