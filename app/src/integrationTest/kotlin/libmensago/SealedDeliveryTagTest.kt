a6d74f63f34dca6657360348b6836f162cc53b39
Jon Yoder <jon@yoder.cloud>
2024-02-28 10:08:07 -0500

    Implement SealedDeliveryTagTest

--- app/src/integrationTest/kotlin/libmensago/SealedDeliveryTagTest.kt
+++ app/src/integrationTest/kotlin/libmensago/SealedDeliveryTagTest.kt
@@ -5,13 +5,20 @@ import keznacl.SecretKey
 import libkeycard.WAddress
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import testsupport.ADMIN_PROFILE_DATA
+import testsupport.FakeDNSHandler
+import testsupport.setupTest
+import java.time.Instant
+import java.util.*
 
 class SealedDeliveryTagTest {
 
     private val gOneAddr = WAddress.fromString(
-        "11111111-1111-1111-1111-111111111111/example.com")!!
+        "11111111-1111-1111-1111-111111111111/example.com"
+    )!!
     private val gTwoAddr = WAddress.fromString(
-        "22222222-2222-2222-2222-222222222222/example.com")!!
+        "22222222-2222-2222-2222-222222222222/example.com"
+    )!!
 
     @Test
     fun basicTest() {
@@ -36,6 +43,37 @@ class SealedDeliveryTagTest {
 
     @Test
     fun readTest() {
+        val setupData = setupTest("sealedDeliveryTag.read")
+
+        val msg = Message(gOneAddr, gTwoAddr, MsgFormat.Text)
+            .setSubject("A Message")
+            .setBody("We're just testing here ")
+
+        val adminPair = EncryptionPair.fromStrings(
+            ADMIN_PROFILE_DATA["encryption.public"]!!,
+            ADMIN_PROFILE_DATA["encryption.private"]!!
+        ).getOrThrow()
+        adminPair.getPublicHash()
+
+        val sealed = Envelope.seal(adminPair, msg, FakeDNSHandler()).getOrThrow()
+
+        val filelength = sealed.toStringResult().getOrThrow().length
+        val filePath = listOf(
+            setupData.testPath,
+            "${Instant.now().epochSecond}.$filelength.${UUID.randomUUID().toString().lowercase()}"
+        ).toPath()
+        sealed.saveFile(filePath.toString())?.let { throw it }
+
+        val tag = SealedDeliveryTag.readFromFile(filePath.toFile()).getOrThrow()
+        assertEquals(tag.type, sealed.tag.type)
+        assertEquals(tag.receiver, sealed.tag.receiver)
+        assertEquals(tag.sender, sealed.tag.sender)
+        assertEquals(tag.payloadKey, sealed.tag.payloadKey)
+        assertEquals(tag.keyHash, sealed.tag.keyHash)
+        assertEquals(tag.date, sealed.tag.date)
+        assertEquals(tag.subType, sealed.tag.subType)
+        assertEquals(tag.version, sealed.tag.version)
+
         // TODO: Implement SealedDeliveryTagTest::readTest()
     }
 }
