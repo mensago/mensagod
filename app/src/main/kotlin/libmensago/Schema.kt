816d77f9152a96fe2a6e3861393bf97e0a1fb5fc
Jon Yoder <jon@yoder.cloud>
2024-03-02 09:33:59 -0500

    Start Schema implementation - Add Schema-related types - Implement Schema::validate()

--- app/src/main/kotlin/libmensago/Schema.kt
+++ app/src/main/kotlin/libmensago/Schema.kt
@@ -1,5 +1,11 @@
 package libmensago
 
+import keznacl.CryptoString
+import libkeycard.Domain
+import libkeycard.MissingFieldException
+import libkeycard.RandomID
+import libkeycard.UserID
+
 /*
     Message validation:
     Check that all required fields exist
@@ -27,12 +33,75 @@ package libmensago
 
     The ideal way of writing this would be:
 
-    val schema = SchemaBuilder {
-        Field { "Workspace-ID", Field.RandomID, true },
-        Field { "User-ID", Field.UserID, false },
-    }
+    val schema = SchemaBuilder(
+        Field("Workspace-ID", Field.RandomID, true),
+        Field("User-ID", Field.UserID, false),
+    )
     schema.validate(state.message.data)
-        ?.let { it.sendCatching("Failed to send validation failure message") }
+        ?.let { QuickResponse.sendBadRequest("Failed to send validation failure message") }
     val wid = schema.getRandomID("Workspace-ID")
 
 */
+
+enum class MsgFieldType {
+    CryptoString,
+    Domain,
+    Integer,
+    Path,
+    RandomID,
+    String,
+    UnixTime,
+    UserID,
+}
+
+/** The MsgField type contains validation information about a message field. */
+data class MsgField(val name: String, val type: MsgFieldType, val req: Boolean)
+
+/**
+ * The Schema class is used for validating attached message data and converting it to the desired
+ * data types.
+ */
+class Schema(vararg args: MsgField) {
+    val fields = args.associateBy { it.name }
+
+    /**
+     * Validates the data map passed to it and executes the handler code if validation should fail
+     */
+    fun validate(data: Map<String, String>, failHandler: (String, Throwable) -> Unit) {
+        for (field in fields.values) {
+
+            if (!data.containsKey(field.name)) {
+                if (field.req) {
+                    failHandler(field.name, MissingFieldException())
+                    return
+                }
+                continue
+            }
+
+            val isValid = when (field.type) {
+                MsgFieldType.CryptoString -> CryptoString.checkFormat(data[field.name]!!)
+                MsgFieldType.Domain -> Domain.checkFormat(data[field.name]!!)
+                MsgFieldType.Integer -> {
+                    val result = runCatching { data[field.name]!!.toInt() }
+                    if (result.isSuccess)
+                        result.getOrNull()!! >= 0
+                    else
+                        false
+                }
+
+                MsgFieldType.Path -> MServerPath.checkFormat(data[field.name]!!)
+                MsgFieldType.RandomID -> RandomID.checkFormat(data[field.name]!!)
+                MsgFieldType.String -> data[field.name]!!.isNotEmpty()
+                MsgFieldType.UnixTime -> {
+                    val result = runCatching { data[field.name]!!.toLong() }
+                    if (result.isSuccess)
+                        result.getOrNull()!! >= 0
+                    else
+                        false
+                }
+
+                MsgFieldType.UserID -> UserID.checkFormat(data[field.name]!!)
+            }
+        }
+    }
+}
