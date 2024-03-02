bedbeed0259ef6b94c8de5607cde2a8f13fecd82
Jon Yoder <jon@yoder.cloud>
2024-03-02 11:34:03 -0500

    Add test stub and TODO for commandGetUpdates()

---
+++ app/src/integrationTest/kotlin/mensagod/handlers/SyncCmdTest.kt
@@ -0,0 +1,11 @@
+package mensagod.handlers
+
+import org.junit.jupiter.api.Test
+
+class SyncCmdTest {
+
+    @Test
+    fun getUpdatesTest() {
+        // TODO: Implement test for getUpdates()
+    }
+}
