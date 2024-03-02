e2a142178419859813f21ed7d9aeaba738c59aa1
Jon Yoder <jon@yoder.cloud>
2024-03-01 19:06:12 -0500

    Implement getSyncRecords and cullOldSyncRecords

--- app/src/main/kotlin/mensagod/dbcmds/Updates.kt
+++ app/src/main/kotlin/mensagod/dbcmds/Updates.kt
@@ -8,6 +8,9 @@ import libkeycard.EntryTypeException
 import libkeycard.RandomID
 import libmensago.MServerPath
 import mensagod.DBConn
+import mensagod.DatabaseCorruptionException
+import mensagod.ServerConfig
+import java.time.Instant
 
 class UpdateRecord(val id: RandomID, val type: UpdateType, val data: String, val time: String,
                    val devid: RandomID)
@@ -89,6 +92,49 @@ fun countSyncRecords(db: DBConn, wid: RandomID, unixtime: Long): Result<Int> {
     return out.toSuccess()
 }
 
+/**
+ * Clears out old sync records older than the amount of time specified in the server config.
+ *
+ * @exception BadValueException Returned if the max sync age could not be obtained from the server
+ * config
+ */
+fun cullOldSyncRecords(db: DBConn, wid: RandomID): Throwable? {
+    val unixtime = Instant.now().epochSecond
+    val maxDays = ServerConfig.get().getInteger("performance.max_sync_age")
+        ?: return BadValueException()
+    val threshold = unixtime - (maxDays.toLong() * 86400)
+    return db.execute("DELETE FROM updates WHERE unixtime - ? > 0", threshold).exceptionOrNull()
+}
+
+/**
+ * Gets all update records after a specified period of time up to a maximum of 1000.
+ */
+fun getSyncRecords(db: DBConn, wid: RandomID, unixtime: Long): Result<List<UpdateRecord>> {
+    if (unixtime < 0) return BadValueException().toFailure()
+
+    val rs = db.query(
+        "SELECT rid,update_type,update_data,unixtime,devid FROM updates WHERE wid=? " +
+                "AND unixtime > ? ORDER BY rowid LIMIT 1000", wid, unixtime
+    )
+        .getOrElse { return it.toFailure() }
+    val out = mutableListOf<UpdateRecord>()
+    while (rs.next()) {
+        val id = RandomID.fromString(rs.getString("rid"))
+            ?: return DatabaseCorruptionException("Invalid update ID").toFailure()
+        val type = UpdateType.fromString(rs.getString("update_type"))
+            ?: return DatabaseCorruptionException("Invalid update type").toFailure()
+        val devid = RandomID.fromString(rs.getString("devid"))
+            ?: return DatabaseCorruptionException("Invalid device ID").toFailure()
+
+        out.add(
+            UpdateRecord(
+                id, type, rs.getString("update_data"), rs.getString("unixtime"),
+                devid
+            )
+        )
+    }
+    return out.toSuccess()
+}
 
 /**
  * The UpdateType enumerated class represents the different possible operations described by
