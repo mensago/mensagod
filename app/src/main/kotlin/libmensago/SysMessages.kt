f172b87c6d2dc20b2e2b2f06098e12127a3a81ee
Jon Yoder <jon@yoder.cloud>
2024-03-06 10:33:07 -0500

    Implement function to extract approval context information from a DeviceApprovalMsg

--- app/src/main/kotlin/libmensago/SysMessages.kt
+++ app/src/main/kotlin/libmensago/SysMessages.kt
@@ -1,6 +1,6 @@
 package libmensago
 
-import keznacl.CryptoString
+import keznacl.*
 import libkeycard.Timestamp
 import libkeycard.WAddress
 import java.net.InetAddress
@@ -25,5 +25,35 @@ class DeviceApprovalMsg(from: WAddress, to: WAddress, addr: InetAddress, devInfo
                 devInfo.toString().encodeToByteArray()
             )
         )
+    }
+
+    companion object {
+
+        /**
+         * Decrypts the attached device info on an approval message and returns the expected message
+         * body to be presented to the user after possible other processing.
+         */
+        fun getApprovalInfo(
+            userPair: EncryptionPair,
+            msg: DeviceApprovalMsg
+        ): Result<Map<String, String>> {
+            if (msg.attachments.size < 1)
+                return MissingDataException("DeviceInfo attachment missing").toFailure()
+
+            val attach = msg.attachments[0]
+            if (attach.name != "deviceinfo" ||
+                attach.mime.toString() != "application/vnd.mensago.encrypted-json"
+            ) {
+                return BadValueException("First attachment is not deviceinfo").toFailure()
+            }
+            val cs = CryptoString.fromString(attach.data.decodeToString())
+                ?: return BadValueException("Bad deviceinfo attachment data").toFailure()
+            val infoStr = userPair.decrypt(cs).getOrElse { return it.toFailure() }.decodeToString()
+
+            return msg.body.trim().split("\r\n")
+                .plus(infoStr.trim().split("\r\n"))
+                .associate { line -> line.trim().split(":").let { Pair(it[0], it[1]) } }
+                .toSuccess()
+        }
     }
 }
