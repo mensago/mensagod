8af0c455b0095128ae32c3eef1af78092b974ae3
Jon Yoder <jon@yoder.cloud>
2024-03-07 16:18:57 -0500

    Add sendOK and actually make the QuickResponse calls suppress exceptions

--- app/src/main/kotlin/mensagod/QuickResponse.kt
+++ app/src/main/kotlin/mensagod/QuickResponse.kt
@@ -16,7 +16,8 @@ object QuickResponse {
      * @throws IllegalArgumentException if the encoded input does not comply format's specification
      */
     fun sendBadRequest(info: String, conn: Socket) {
-        ServerResponse(400, "BAD REQUEST", info).send(conn)
+        ServerResponse(400, "BAD REQUEST", info)
+            .sendCatching(conn, "Error sending quick Bad Request")
     }
 
     /**
@@ -27,7 +28,8 @@ object QuickResponse {
      * @throws IllegalArgumentException if the encoded input does not comply format's specification
      */
     fun sendForbidden(info: String, conn: Socket) {
-        ServerResponse(403, "FORBIDDEN", info).send(conn)
+        ServerResponse(403, "FORBIDDEN", info)
+            .sendCatching(conn, "Error sending quick Forbidden")
     }
 
     /**
@@ -38,8 +40,22 @@ object QuickResponse {
      * @throws IllegalArgumentException if the encoded input does not comply format's specification
      */
     fun sendInternalError(info: String, conn: Socket) {
-        ServerResponse(300, "INTERNAL SERVER ERROR", info).send(conn)
+        ServerResponse(300, "INTERNAL SERVER ERROR", info)
+            .sendCatching(conn, "Error sending quick Internet Error")
     }
+
+    /**
+     * Static method which sends a 200 OK message to the client. It suppresses network errors
+     * because we're already in an error state.
+     *
+     * @throws kotlinx.serialization.SerializationException encoding-specific errors
+     * @throws IllegalArgumentException if the encoded input does not comply format's specification
+     */
+    fun sendOK(info: String, conn: Socket) {
+        ServerResponse(200, "OK", info)
+            .sendCatching(conn, "Error sending quick OK")
+    }
+
 
     /**
      * Static method which sends a 404 NOT FOUND message to the client. It suppresses network errors
@@ -49,7 +65,8 @@ object QuickResponse {
      * @throws IllegalArgumentException if the encoded input does not comply format's specification
      */
     fun sendNotFound(info: String, conn: Socket) {
-        ServerResponse(404, "RESOURCE NOT FOUND", info).send(conn)
+        ServerResponse(404, "RESOURCE NOT FOUND", info)
+            .sendCatching(conn, "Error sending quick Not Found")
     }
 }
 
@@ -62,8 +79,7 @@ fun ServerResponse.sendCatching(conn: Socket, message: String): Boolean {
     try {
         val jsonStr = Json.encodeToString(this)
         writeMessage(conn.getOutputStream(), jsonStr.encodeToByteArray())
-    }
-    catch (e: Exception) {
+    } catch (e: Exception) {
         logDebug("$message: $e")
         return false
     }
