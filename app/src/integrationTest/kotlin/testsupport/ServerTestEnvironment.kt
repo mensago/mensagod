27f9d8d65eb8e63e65070c77fec89d396edad1ea
Jon Yoder <jon@yoder.cloud>
2024-03-23 16:05:26 -0400

    Finish filesystem-level provisioning and add passing integration test

--- app/src/integrationTest/kotlin/testsupport/ServerTestEnvironment.kt
+++ app/src/integrationTest/kotlin/testsupport/ServerTestEnvironment.kt
@@ -1,7 +1,13 @@
 package testsupport
 
-import mensagod.PGConfig
-import mensagod.ServerConfig
+import libkeycard.Domain
+import libkeycard.WAddress
+import libmensago.MServerPath
+import libmensago.resolver.KCResolver
+import mensagod.*
+import org.apache.commons.io.FileUtils
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import java.nio.file.Paths
 
 /*
@@ -12,27 +18,81 @@ import java.nio.file.Paths
     Some tests will need mock server-side data and some will not. This needs to support both.
  */
 
+const val SETUP_TEST_FILESYSTEM: Int = 1
 
 /**
  * This support class is responsible for setting up the server side of anenvironment for integration
  * tests.
  */
-class ServerTestEnvironment() {
-    val dbconfig: PGConfig
-    val serverconfig: ServerConfig
-    val testPath: String
+class ServerTestEnvironment(val testName: String) {
+    private val dbconfig: PGConfig = PGConfig()
+    val serverconfig: ServerConfig = ServerConfig.load().getOrThrow()
+    val testPath: String = Paths.get("build", "testfiles", testName).toAbsolutePath()
+        .toString()
 
-    init {
-        dbconfig = PGConfig()
-        serverconfig = ServerConfig.load().getOrThrow()
-        testPath = Paths.get("build", "testfiles").toAbsolutePath().toString()
+    /**
+     * Provisions the test environment based on the configuration set
+     */
+    fun provision(provisionLevel: Int): ServerTestEnvironment {
+        setupTestBase()
+
+        return this
     }
 
+    /**
+     * Provisions the integration test folder hierarchy and environment baseline. The test root
+     * contains one subfolder, topdir, which is the server's workspace data folder. This also
+     * provides other filesystem-related setup, such as initializing logging, LocalFS, and the
+     * mock DNS handler.
+     */
+    private fun setupTestBase() {
+        val topdir = Paths.get(testPath, "topdir")
+        val topdirFile = topdir.toFile()
+        if (topdirFile.exists())
+            FileUtils.cleanDirectory(topdirFile)
+        else
+            topdirFile.mkdirs()
 
-    // Data supplied:
-    // test name
+        listOf("wsp", "out", "tmp", "keys").forEach {
+            Paths.get(topdir.toString(), it).toFile().mkdir()
+        }
 
-    // Data needed from build environment:
-    // Top folder for test
-    // How to connect to Postgres
+        initLogging(Paths.get(testPath, "log.txt"), true)
+        LocalFS.initialize(topdir.toString())?.let { throw it }
+        KCResolver.dns = FakeDNSHandler()
+        gServerDomain = Domain.fromString(serverconfig.getString("global.domain"))!!
+        gServerAddress = WAddress.fromParts(gServerDevID, gServerDomain)
+    }
+}
+
+
+class ServerEnvironmentTest {
+    @Test
+    fun testBaseSetup() {
+        val env = ServerTestEnvironment("servertestenv").provision(SETUP_TEST_FILESYSTEM)
+
+        val lfs = LocalFS.get()
+        val rootPath = MServerPath()
+        assertEquals(
+            lfs.convertToLocal(rootPath).toString(),
+            Paths.get(env.testPath, "topdir").toString()
+        )
+        assert(rootPath.toHandle().exists().getOrThrow())
+        listOf("wsp", "out", "tmp", "keys").forEach {
+            val path = MServerPath("/ $it")
+            if (!path.toHandle().exists().getOrThrow())
+                throw TestFailureException("Server workspace dir '/ $it' doesn't exist")
+        }
+
+        val exampleIPs = KCResolver.dns
+            .lookupA("example.com")
+            .getOrThrow()
+        assertEquals(1, exampleIPs.size)
+        assertEquals("/127.0.0.1", exampleIPs[0].toString())
+
+        val oldLevel = getLogLevel()
+        setLogLevel(LOG_INFO)
+        logInfo("${env.testName} log test entry")
+        setLogLevel(oldLevel)
+    }
 }
